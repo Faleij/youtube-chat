8e3c0802333c2e11bddf0873cc4abbcdc6d34fed
Faleij <faleij@live.se>
2025-03-30 19:44:54 +0200

    Use axios instance instead of polluting default options

--- src/requests.ts
+++ src/requests.ts
@@ -3,11 +3,16 @@ import { parseChatData, getOptionsFromLivePage } from "./parser"
 import { FetchOptions } from "./types/yt-response"
 import { ChatItem, YoutubeId } from "./types/data"
 
-axios.defaults.headers.common["Accept-Encoding"] = "utf-8"
+const client = axios.create({
+  baseURL: 'https://www.youtube.com/',
+  headers: {
+    "Accept-Encoding": "utf-8",
+  },
+})
 
 export async function fetchChat(options: FetchOptions): Promise<[ChatItem[], string]> {
-  const url = `https://www.youtube.com/youtubei/v1/live_chat/get_live_chat?key=${options.apiKey}`
-  const res = await axios.post(url, {
+  const url = `youtubei/v1/live_chat/get_live_chat?key=${options.apiKey}`
+  const res = await client.post(url, {
     context: {
       client: {
         clientVersion: options.clientVersion,
@@ -25,21 +30,21 @@ export async function fetchLivePage(id: { channelId: string } | { liveId: string
   if (!url) {
     throw TypeError("not found id")
   }
-  const res = await axios.get(url)
+  const res = await client.get(url)
   return getOptionsFromLivePage(res.data.toString())
 }
 
 function generateLiveUrl(id: YoutubeId) {
   if ("channelId" in id) {
-    return `https://www.youtube.com/channel/${id.channelId}/live`
+    return `channel/${id.channelId}/live`
   } else if ("liveId" in id) {
-    return `https://www.youtube.com/watch?v=${id.liveId}`
+    return `watch?v=${id.liveId}`
   } else if ("handle" in id) {
     let handle = id.handle
     if (!handle.startsWith("@")) {
       handle = "@" + handle
     }
-    return `https://www.youtube.com/${handle}/live`
+    return `${handle}/live`
   }
   return ""
 }
